fa3f75d614eead71fa7b4b091ea4bc34084427b9
nmred <nmred_2008@126.com>
2017-02-15 18:14:13 +0800

    update docs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adbase
-Weibo ad C++ framework
+adbase - Weibo ad C++ framework
+==================================================
+
+Copyright (c) 2009-2017, [Weiboad](http://www.weibo.com/).
+
+[https://github.com/weiboad/adbase](https://github.com/weiboad/adbase)
--- docs/search_config.php
+++ docs/search_config.php
@@ -16,7 +16,7 @@ $translator = array(
   'search_matches' => "Matches:",
   'search' => "Search",
   'split_bar' => "<div id=\"side-nav\" class=\"ui-resizable side-nav-resizable\">\n  <div id=\"nav-tree\">\n    <div id=\"nav-tree-contents\">\n      <div id=\"nav-sync\" class=\"sync\"></div>\n    </div>\n  </div>\n  <div id=\"splitbar\" style=\"-moz-user-select:none;\" \n       class=\"ui-resizable-handle\">\n  </div>\n</div>\n<script type=\"text/javascript\">\n$(document).ready(function(){initNavTree('search.html','');});\n</script>\n<div id=\"doc-content\">\n",
-  'logo' => "Generated on Wed Feb 15 2017 17:56:24 for Adbase by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.11 ",
+  'logo' => "Generated on Wed Feb 15 2017 18:13:50 for Adbase by&#160;\n<a href=\"http://www.doxygen.org/index.html\">\n<img class=\"footer\" src=\"doxygen.png\" alt=\"doxygen\"/></a> 1.8.11 ",
 );
 
 </script>
