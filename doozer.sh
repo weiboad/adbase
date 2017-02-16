2e067e7862befbb0208e3a6d497c7e4cd74aa491
nmred <nmred_2008@126.com>
2017-02-16 18:36:56 +0800

    fix doozer.json

--- doozer.sh
+++ doozer.sh
@@ -11,3 +11,8 @@ cd /usr/src/gtest
 sudo cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8 .
 sudo make
 sudo cp libgtest*.a /usr/local/lib
+
+cd /project/repo/checkout
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DM32=0 -DUSEKAFKA=0 -DADINFVERSION=1 -DADINFSOVERSION=1 -DDEV=1 -DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8 ..
