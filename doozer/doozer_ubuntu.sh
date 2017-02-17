01fb3e6878ecae4c894b27c5e275cfe24eb147b7
nmred <nmred_2008@126.com>
2017-02-17 09:37:20 +0800

    fix

--- doozer/doozer_ubuntu.sh
+++ doozer/doozer_ubuntu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-sudo wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
-sudo tar -zxf libevent-2.1.8-stable.tar.gz
+wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+tar -zxf libevent-2.1.8-stable.tar.gz
 cd libevent-2.1.8-stable 
-sudo ./configure 
-sudo make 
-sudo make install
+./configure 
+make 
+make install
 
 # install gtest
 cd /usr/src/gtest
-sudo cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8 .
-sudo make
-sudo cp libgtest*.a /usr/local/lib
+cmake -DCMAKE_CXX_COMPILER=/usr/bin/g++-4.8 .
+make
+cp libgtest*.a /usr/local/lib
 
 cd /project/repo/checkout
 mkdir build
